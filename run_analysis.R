455bc14728ead8ad1b14f635b52531a5e8dd75fc
Jeremiah Shirk <jeremiah.shirk@venmo.com>
2014-06-22 10:44:35 -0500

    Add comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,11 @@
+### run_analysis.R
+### Coursera Data Science - Getting and Cleaning Data
+### Course Project
+
+# You should create one R script called run_analysis.R that does the following. 
+# 1. Merges the training and the test sets to create one data set.
+
+# Load the data from the HAR data set
 activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
 features <- read.table("UCI HAR Dataset/features.txt")
 x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
@@ -7,14 +15,15 @@ x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
 subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
 
-# You should create one R script called run_analysis.R that does the following. 
-# 1. Merges the training and the test sets to create one data set.
+# Merge the data into a new, combined data frame
 x.df <- rbind(x_test, x_train)
 y.df <- rbind(y_test, y_train)
 subject.df <- rbind(subject_test, subject_train)
 
 # 4. Appropriately labels the data set with descriptive variable names. 
 # Clean up the variable names from features, and apply them to x.df
+# NB: this could also be done between steps (3) and (5) by limiting
+# the cleaned data_labels to the columns selected in (2).
 data_labels = sapply(features$V2, function(x) {gsub('[-(),]+','_', x)})
 colnames(x.df) <- data_labels
 
